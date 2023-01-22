b394e4d1a885e8760cfbc15f8faa92fd8225b140
scryvener <71054893+scryvener@users.noreply.github.com>
2023-01-22 12:37:01 -0800

    Trying to time functions

--- Pages/form_funct.js
+++ Pages/form_funct.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
     var current_fs, next_fs, previous_fs; //fieldsets
@@ -7,32 +9,37 @@ $(document).ready(function(){
     
     setProgressBar(current);
     
-    $(".next").click(function(){
+    $(".next").click(
+        
+        function(){
+
+            submitWrap()
     
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
-        
-        //Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-        
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function(now) {
-                // for making fielset appear animation
-                opacity = 1 - now;
-                
-                current_fs.css({
-                'display': 'none',
-                'position': 'relative'
-                });
-                next_fs.css({'opacity': opacity});
-            },
-            duration: 500
-        });
-        setProgressBar(++current);
-    });
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+            
+            //Add Class Active
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+            
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+                step: function(now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+                    
+                    current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                    });
+                    next_fs.css({'opacity': opacity});
+                },
+                duration: 500
+            });
+            setProgressBar(++current);
+        }
+    );
     
     $(".previous").click(function(){
     
@@ -73,4 +80,4 @@ $(document).ready(function(){
         return false;
     })
     
-    });
+});
