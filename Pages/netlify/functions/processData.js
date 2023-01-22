ca530905ade11549326029bea994bcfc42e97a16
scryvener <71054893+scryvener@users.noreply.github.com>
2023-01-21 22:30:59 -0800

    Changes
    Trying to fix bot honeypot, testing json

--- Pages/netlify/functions/processData.js
+++ Pages/netlify/functions/processData.js
@@ -3,12 +3,19 @@ export const handler = async (event,data) => {
     
     var inc=JSON.parse(event.body)
 
+    var openR1=inc.openR+'Hello There'
+
+    var product1=inc.product+'you found me'
+
     //var i=5;
     //var k=i+Number(inc.someData)
     // Netlify Functions need to return an object with a statusCode
     // Other properties such as headers or body can also be included.
     return {
         statusCode: 200,
-        body: JSON.stringify(inc)
+        body: {
+            'responseMod':openR1,
+            'product1Mod':product1
+        }
     }
 }
